ffbda98f0e9bbb8604a0b31402e04fa6e4e5779a
Marcos <lapiramarcos@gmial.com>
2025-11-28 18:12:38 -0300

    modificacion en la obtenecion de categorias

--- Frontend/public/ticket/ticket.js
+++ Frontend/public/ticket/ticket.js
@@ -1,27 +1,23 @@
-
-
-//leemos el id del ultimol ticket guarddado
+//obtencion del dom
 const dataJSON = localStorage.getItem("ultimoTicket");
+const ticket = JSON.parse(dataJSON);
+const spanNombreCliente = document.getElementById("ticketNombreCliente");
+const spanFecha = document.getElementById("ticketFecha");
+const spanIdVenta = document.getElementById("ticketIdVenta");
+const spanTotal = document.getElementById("ticketTotal");
+const tbodyDetalle = document.getElementById("ticketDetalle");
+const btnNuevoPedido = document.getElementById("btnNuevoPedido");
+const btnDescargarTicket = document.getElementById("btnDescargarTicket");
 
 if (!dataJSON) {
 
   // Si no hay data, volvemos a la pantalla del cliente
-  alert("No hay información de ticket. Volviendo a la tienda.");
+  alert("No hay información de ticket.Redirigiendo al inicio");
   window.location.href = "/public/inicio/index.html";
 
 } else {
 
-  const ticket = JSON.parse(dataJSON);
 
-  const spanNombreCliente = document.getElementById("ticketNombreCliente");
-  const spanFecha = document.getElementById("ticketFecha");
-  const spanIdVenta = document.getElementById("ticketIdVenta");
-  const spanTotal = document.getElementById("ticketTotal");
-  const tbodyDetalle = document.getElementById("ticketDetalle");
- 
-
-
- 
   nombreEmpresa.textContent = "MateGo - Autoservicio de mates, yerbas y mas.";
 
   spanNombreCliente.textContent = ticket.nombreCliente || "-";
@@ -37,10 +33,10 @@ if (!dataJSON) {
 
   let totalCalculado = 0;
 
+  // recorremos los items del ticket
   (ticket.items || []).forEach((item) => {
 
     const tr = document.createElement("tr");
-
     const precio = Number(item.precio) || 0;
     const cantidad = Number(item.cantidad) || 0;
     const subtotal = precio * cantidad;
@@ -62,20 +58,20 @@ if (!dataJSON) {
 
 }
 
-// Botones
-const btnNuevoPedido = document.getElementById("btnNuevoPedido");
-const btnDescargarTicket = document.getElementById("btnDescargarTicket");
 
 // volvemos al inicio
 btnNuevoPedido.addEventListener("click", () => {
   
   //lim piamos el localStorage
-   localStorage.removeItem("ultimoTicket");
+  localStorage.removeItem("ultimoTicket");
   window.location.href = "/public/inicio/index.html";
+
 });
 
 
 // descargar ticket
 btnDescargarTicket.addEventListener("click", () => {
-  alert("Mas adelante la hago");
+
+  alert("Funcion disponible mas adelante cuadno la afip me investigue por evasion impositiva. No me atraparan!");
+
 });
